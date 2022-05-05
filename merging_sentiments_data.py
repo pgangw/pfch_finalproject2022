acd152e66420c17fc804e285a66177c63ff25431
pgangw <102167360+pgangw@users.noreply.github.com>
2022-05-05 12:00:55 -0400

    Add files via upload

--- merging_sentiments_data.py
+++ merging_sentiments_data.py
@@ -6,6 +6,9 @@ title_df = pd.read_csv('title_sentiments.csv')
 description_df = pd.read_csv('description_sentiments.csv')
 tandd_df = pd.read_csv('roundone.csv')
 
+#roundone.csv is the file that was first created by joining title and description
+# That combined file was then joined with transcripts 
+
 allsentiments_df=pd.merge(tandd_df, transcripts_df, on="url", how="outer", indicator="true")
 allsentiments_df.to_csv("master_all_sentiments.csv", index=False, encoding='UTF8')
 
